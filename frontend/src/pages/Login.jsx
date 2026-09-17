60af5ffde0ff10548344a7a9c6951be3b794e0d1
beneyamlegese12-coder <beneyam.legese12@gmail.com>
2026-09-17 15:15:09 +0300

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 ﻿import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/authContext';
+import { useAuth } from './context/AuthContext';
 import { FaEye, FaEyeSlash, FaUser, FaLock, FaGamepad } from 'react-icons/fa';
 
 const Login = () => {
